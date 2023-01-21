001acb5b6dfe276d5339f3bd698d9cf3615bad97
Wes Pittman <wesley.a.pittman@gmail.com>
2023-01-21 15:46:21 -0500

    Updates

--- Conversion.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Read the Excel file
-df = pd.read_excel(
-    'C:\\Users\\pittm\\Programming\\Kaggle\\FWP-CountryData.xlsx')
-
-# Write the data frame to a CSV file
-df.to_csv('FWP-CountryData.csv', index=False)
